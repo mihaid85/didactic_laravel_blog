9a6a031fb15311a06ac5e94568c794e748a84967
mihai <mihaidanet@gmail.com>
2017-05-25 15:33:37 +0300

    learning

--- public/js/script.js
+++ public/js/script.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
                 scrollTop: 0
             }, 800);
             return false;
-
+        });
             /* validation */
     $("#register-form").validate({
         rules:
@@ -105,40 +105,8 @@ $(document).ready(function(){
         return false;
     }
     /* form submit */
-    });
+    
 
-    //Dropzone.js Options - Upload an image via AJAX.
-      Dropzone.options.myDropzone = {
-        uploadMultiple: false,
-        // previewTemplate: '',
-        addRemoveLinks: false,
-        // maxFiles: 1,
-        dictDefaultMessage: '',
-        init: function() {
-          this.on("addedfile", function(file) {
-            // console.log('addedfile...');
-          });
-          this.on("thumbnail", function(file, dataUrl) {
-            // console.log('thumbnail...');
-            $('.dz-image-preview').hide();
-            $('.dz-file-preview').hide();
-          });
-          this.on("success", function(file, res) {
-            console.log('upload success...');
-            $('#img-thumb').attr('src', res.path);
-            $('input[name="pic_url"]').val(res.path);
-          });
-        }
-      };
-      var myDropzone = new Dropzone("#my-dropzone");
-     
-      $('#upload-submit').on('click', function(e) {
-        e.preventDefault();
-        //trigger file upload select
-        $("#my-dropzone").trigger('click');
-      });
- 
+    
 });
 
-//we want to manually init the dropzone.
-Dropzone.autoDiscover = false;
